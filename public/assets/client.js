1cc280448497270e56eb12a80ab76d3bd3dbe650
Quo0 <intestinesonastick@gmail.com>
2018-07-01 18:11:47 +0400

    Added confirmitions to all user's actions.

--- public/assets/client.js
+++ public/assets/client.js
@@ -57,10 +57,11 @@ $('document').ready(()=>{
       success: function(serverResponse){
         let serverArray = serverResponse.dataArr;
         serverArray.sort(sortTodoItems);
-        const ul = $('#todo-list');
-        ul.html('');
+
+        const todoList = $('#todo-list');
+        todoList.html('');
         serverArray.forEach((dataObj)=>{
-          ul.append(`<li class="item-block" data-id="${dataObj._id}">
+          todoList.append(`<li class="item-block" data-id="${dataObj._id}">
                         <p class="item-name">${dataObj.item}</p>
                         <div class="item-description">
                           <p>${dataObj.description}</p>
@@ -84,10 +85,11 @@ $('document').ready(()=>{
           if(a.date > b.date){return 1};
           if(a.date < b.date){return -1};
         });
+
         const doneList = $("#done-list");
         doneList.html("");
         serverArray.forEach((dataItem)=>{
-          doneList.append(`<li class="item-block data-id="${dataItem._id}">
+          doneList.append(`<li class="item-block" data-id="${dataItem._id}">
                               <p class="item-name">${dataItem.item}</p>
                               <div class="item-description">
                                 <p>${dataItem.description}</p>
@@ -106,8 +108,19 @@ $('document').ready(()=>{
     event.preventDefault();
     const newItemInput = $('#newItem');
     const newTodo = newItemInput.val();
-    const newTodoDescr = prompt("Any description?","");
-    newItemInput.val("");
+    if(newTodo == false){
+      alert("Please, enter the name!")
+      newItemInput.focus();
+      return;
+    };
+    const newTodoDescr = (function(){
+      let answer = prompt("Any description?","");
+      if(answer == null || answer == false){
+        return "without description";
+      } else {
+        return answer;
+      };
+    })();
 
     $.ajax({
       type: "POST",
@@ -131,91 +144,127 @@ $('document').ready(()=>{
     if(event.target.className == "edit"){
       const itemBlock = event.target.parentNode.parentNode;
       const todoName = itemBlock.querySelector('.item-name').innerHTML
+      const prevDescr = itemBlock.querySelector('.item-description p').innerHTML
 
-      const newName = prompt("Enter new todo name",todoName);
-      const newDescr = prompt("Enter item description", "");
-      let ID = itemBlock.dataset.id;
+      const clientDataPromise = new Promise((resolve,reject)=>{
+        const clientData = {};
 
-      const clientData = {
-        item: newName,
-        description: newDescr
-      };
-
-      $.ajax({
-        type: "PUT",
-        url: `/edit_todo_item/${ID}`,
-        contentType:"application/JSON",
-        data: JSON.stringify(clientData),
-        success: function(){
-          $('#showData').click();
+        const newName = prompt("Enter new todo name",todoName);
+        if(newName == false){
+          reject("Please enter the new name!")
+          return;
+        } else if(newName == null){
+          reject("You didn't change anything");
+          return;
+        } else {
+          clientData.name = newName
         }
+        const newDescr = prompt("Enter item description", prevDescr);
+        if(newDescr == null || newDescr == false){
+          clientData.description = "without description"
+        } else{
+          clientData.description = newDescr;
+        }
+        resolve(clientData);
       });
+
+      clientDataPromise.then(
+        (resolve)=>{
+          const ID = itemBlock.dataset.id;
+          const clientData = {
+            item: resolve.name,
+            description: resolve.description
+          };
+
+          $.ajax({
+            type: "PUT",
+            url: `/edit_todo_item/${ID}`,
+            contentType:"application/JSON",
+            data: JSON.stringify(clientData),
+            success: function(){
+              $('#showData').click();
+            }
+          });
+
+        },
+        (reject)=>{
+          alert(reject);
+      }); 
     }
   });
 
   // DELETE todo
    $('#todo-list').on('click',(event)=>{
     if(event.target.className == "delete"){
-      const itemBlock = event.target.parentNode.parentNode;
-      const todoNameId = itemBlock.querySelector('.item-name').innerHTML.replace(/ /,"-");
+      const confirmed = confirm("Are you sure you want to delete this task?");
+      if(confirmed){
+        const itemBlock = event.target.parentNode.parentNode;
+        const todoNameId = itemBlock.querySelector('.item-name').innerHTML.replace(/ /,"-");
 
-      $.ajax({
-        type: "DELETE",
-        url: `/delete_todo_item/${todoNameId}`,
-        contentType:"application/JSON",
-        data: JSON.stringify({id:todoNameId}),
-        success: function(){
-          $('#showData').click();
-        }
-      });
+        $.ajax({
+          type: "DELETE",
+          url: `/delete_todo_item/${todoNameId}`,
+          contentType:"application/JSON",
+          data: JSON.stringify({id:todoNameId}),
+          success: function(){
+            $('#showData').click();
+          }
+        });
+      }
     }
   });
 
   // MOVE item from Todo to Done
    $('#todo-list').on('click',(event)=>{
     if(event.target.className == "done"){
-      const itemBlock = event.target.parentNode.parentNode;
-      const todoNameId = itemBlock.querySelector('.item-name').innerHTML.replace(/ /g,"-");
+      const confirmed = confirm("Mark this task as done?");
+      if(confirmed){
+        const itemBlock = event.target.parentNode.parentNode;
+        const todoNameId = itemBlock.querySelector('.item-name').innerHTML.replace(/ /g,"-");
 
-      $.ajax({
-        type:"GET",
-        url:`/find_to_delete/${todoNameId}`,
-        contentType:"application/JSON",
-        success: function(itemToDelete){
-          const doneItem = Object.assign({}, itemToDelete.dataArr);
-          const date = formatDate(new Date());
-          doneItem.date = date;
-          $.ajax({
-            type:"POST",
-            url:`/new_done_item`,
-            contentType: 'application/JSON',
-            data: JSON.stringify(doneItem),
-            success: function(data){
-              $('#showData').click();              
-            }
-          });
+        $.ajax({
+          type:"GET",
+          url:`/find_to_delete/${todoNameId}`,
+          contentType:"application/JSON",
+          success: function(itemToDelete){
+            const doneItem = Object.assign({}, itemToDelete.dataArr);
+            const date = formatDate(new Date());
+            doneItem.date = date;
+            $.ajax({
+              type:"POST",
+              url:`/new_done_item`,
+              contentType: 'application/JSON',
+              data: JSON.stringify(doneItem),
+              success: function(data){
+                $('#showData').click();              
+              }
+            });
 
-        }
-      });
+          }
+        });
+      };      
     }
   });
 
   // Remove Done item
   $('#done-list').on('click',(event)=>{
     if(event.target.className == "delete"){
-      const itemBlock = event.target.parentNode.parentNode;
-      const doneItemName = itemBlock.querySelector(".item-name").innerHTML.replace(/ /g,"-");
+      const confirmed = confirm("Are you sure you want to delete this task?");
+      if(confirmed){
+        const itemBlock = event.target.parentNode.parentNode;
+        const doneItemName = itemBlock.querySelector(".item-name").innerHTML.replace(/ /g,"-");
 
-      $.ajax({
-        type:"DELETE",
-        url:`/delete_done_item/${doneItemName}`,
-        contentType:"application/JSON",
-        success: function(){
-          $('#showData').click();
-        }
-      });
+        $.ajax({
+          type:"DELETE",
+          url:`/delete_done_item/${doneItemName}`,
+          contentType:"application/JSON",
+          success: function(){
+            $('#showData').click();
+          }
+        });
+      }
     }
-  })
+  });
   //uploading current data
   $('#showData').click();
 });
